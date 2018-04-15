f616d03cf32e85a3be728d4b64baec1e7d6597a1
Pavlo Dmytrenko <mail@pavdmyt.com>
2018-04-15 22:48:14 +0300

    add solution to problem #167

---
+++ 167-two-sum-II/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                j -= 1
+        raise RuntimeError('Not found')
